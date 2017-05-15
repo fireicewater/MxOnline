e7a9a3b1bb0d88813eca89503fb99a1f04834eed
naiqi <fireicewater001@163.com>
2017-05-15 11:25:31 +0800

    xadmin 配置model及配置全局设置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,6 +1,19 @@
 import xadmin
 from users.models import EmailVerifyRecord, Banner
+from xadmin import views
 
+
+# 全局设置
+class BaseSetting(object):
+    # 主题功能开启
+    enable_themes=True
+    use_bootswatch=True
+# 页面页头页脚
+class GlobalSetting(object):
+    site_title="暮雪后台管理系统"
+    site_footer="暮雪在线网"
+    # 菜单收起
+    menu_style="accordion"
 
 class EmailVerifyRecordAdmin (object):
     list_display=["code","email","send_type","send_time"]
@@ -15,4 +28,7 @@ class BannerAdmin(object):
 
 xadmin.site.register (EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register (Banner, BannerAdmin)
-
+# 注册全局views
+xadmin.site.register(views.BaseAdminView,BaseSetting)
+# 页面页头页脚
+xadmin.site.register(views.CommAdminView,GlobalSetting)
